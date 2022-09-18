57fbbe4be039151165c2d37bc46208c2f71d763c
ibado <ignaciobado.unc@gmail.com>
2022-09-18 19:42:59 -0300

    make router struct public

--- router/router.go
+++ router/router.go
@@ -4,34 +4,34 @@ import (
 	"net/http"
 )
 
-type router struct {
+type Router struct {
 	mux         *http.ServeMux
 	middlewares []Middleware
 }
 
 type Middleware func(next http.Handler) http.Handler
 
-func NewRouter() router {
-	return router{http.NewServeMux(), []Middleware{}}
+func NewRouter() Router {
+	return Router{http.NewServeMux(), []Middleware{}}
 }
 
-func (r *router) Use(middleware Middleware) {
+func (r *Router) Use(middleware Middleware) {
 	r.middlewares = append(r.middlewares, middleware)
 }
 
-func (r router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (r Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.mux.ServeHTTP(w, req)
 }
 
-func (r *router) Handle(path string, handler http.Handler) {
+func (r *Router) Handle(path string, handler http.Handler) {
 	r.registerHandler(path, handler)
 }
 
-func (r *router) HandleFunc(p string, handler func(http.ResponseWriter, *http.Request)) {
+func (r *Router) HandleFunc(p string, handler func(http.ResponseWriter, *http.Request)) {
 	r.registerHandler(p, http.HandlerFunc(handler))
 }
 
-func (r *router) registerHandler(path string, handler http.Handler) {
+func (r *Router) registerHandler(path string, handler http.Handler) {
 	middlewares := r.middlewares
 	r.mux.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
 		h := resolveHandler(middlewares, handler)
